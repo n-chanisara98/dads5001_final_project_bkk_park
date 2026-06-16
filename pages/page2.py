90e9962164ad48428eeb61a93aa0321ffeb8b2e6
n-chanisara98 <chanisara18@gmail.com>
2026-06-16 18:56:59 +0700

    Create page2.py

---
+++ pages/page2.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title("Map - Park finder")
+st.write("Hello Page 2 - หน้านี้ของ [ชื่อเพื่อนคนที่ 2] สำหรับ Plotly พลอตแผนที่ปักหมุดสวนสาธารณะในกรุงเทพฯ เมื่อกด หรือ hover ก็จะบอกรายละเอียดของสวนนั้นๆ")
