c3ceaf8fe69b65f5a0d83a3ae56829542699626b
n-chanisara98 <chanisara18@gmail.com>
2026-06-17 12:15:50 +0700

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -9,7 +9,7 @@ from scipy.spatial.distance import cdist
 import snowflake.connector
 
 st.set_page_config(layout="wide")
-st.title("🌲 Park Finder & Air Quality Monitor (with DuckDB)")
+st.title("🌲 Park Finder & Air Quality Monitor")
 st.subheader("ค้นหาสวนสาธารณะที่เหมาะกับคุณ และเช็กค่าฝุ่น PM2.5 ล่าสุดก่อนออกไปวิ่ง")
 
 # =====================================================================
@@ -48,7 +48,7 @@ mongo_col = mongo_db["pm25"]
 
 def refresh_and_get_pm25():
     # 🌟 เปลี่ยนจาก http เป็น https เพื่อให้ผ่านการบล็อกเน็ตเวิร์กของ Streamlit Cloud
-    api_url = "https://air4thai.pcd.go.th/services/getNewAQI_JSON.php?region=1"
+    api_url = "http://air4thai.pcd.go.th/services/getNewAQI_JSON.php?region=1"
     try:
         # 🌟 เติม verify=False เพื่อข้ามการตรวจใบรับรอง ป้องกันการเชื่อมต่อถูกตัด
         response = requests.get(api_url, timeout=5, verify=False)
