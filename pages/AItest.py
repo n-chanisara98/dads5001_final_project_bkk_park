bec259e76f7a96841cd8d1beda91fa126af8c15a
n-chanisara98 <chanisara18@gmail.com>
2026-06-17 21:33:50 +0700

    Update AItest.py

--- pages/AItest.py
+++ pages/AItest.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 genai.configure(
-    api_key=st.secrets[google]["GEMINI_API_KEY"]
+    api_key=st.secrets["google"]["GEMINI_API_KEY"]
 )
 
 model = genai.GenerativeModel("gemini-2.5-flash")
