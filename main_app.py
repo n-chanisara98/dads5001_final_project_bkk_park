967a745ccb42021d32995c9ff38faac34ec04d4c
n-chanisara98 <chanisara18@gmail.com>
2026-06-17 14:12:32 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="ASEAN Urban & Air Quality Dashboard", layout="wide")
+st.set_page_config(page_title="BKK Park & Air Quality Dashboard", layout="wide")
 
 st.title("🏙️ ASEAN Urban Growth & Air Quality Project")
-st.write("ยินดีต้อนรับเข้าสู่แดชบอร์ดวิเคราะห์ข้อมูลเมือง พิกัดสวนสาธารณะ และค่าฝุ่น PM2.5")
+st.write("ยินดีต้อนรับเข้าสู่แดชบอร์ดวิเคราะห์ข้อมูลสวนสาธารณะใน กทม. และค่าฝุ่น PM2.5")
 
 st.info("👈 กรุณาเลือกหน้าต่างที่ต้องการวิเคราะห์จากเมนูด้านซ้ายได้เลยครับ")
 
