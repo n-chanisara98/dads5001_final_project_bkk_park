42ada981ec184f94dea482758d11c2da56936a9f
n-chanisara98 <chanisara18@gmail.com>
2026-06-17 21:25:13 +0700

    Create AItest.py

---
+++ pages/AItest.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import google.generativeai as genai
+
+genai.configure(
+    api_key=st.secrets["GEMINI_API_KEY"]
+)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+st.title("AI Test")
+
+question = st.text_input("Ask something")
+
+if st.button("Run"):
+    response = model.generate_content(question)
+    st.write(response.text)
